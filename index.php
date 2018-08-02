00b6924c7efd031bc1f6dae1719bbb9252ac48c1
Cavatz Natacha <natacha.cavatz@gmail.com>
2018-08-02 14:12:27 +0200

    Création de la page d'accueil + création dossier MVC

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Billet pour l'Alaska</title>
+	<title><?= $title ?></title>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="public/css/style.css">
 </head>
 <body>
 	<header>
 		<div id="navbar">
-			<a href="#"><img src="images/logo_alaska.png"></a>
+			<a href="#"><img src="public/images/logo_alaska.png"></a>
 			<i id="hamburger" class="fas fa-bars"></i>
 			<i id="cross" class="fas fa-times"></i>
 		</div>
@@ -26,8 +26,66 @@
 			<h2>Par Jean Forteroche</h2>
 		</div>
 	</header>
+	
+	<section id="about">
+		<div id="text_about">
+			<h2>A propos</h2>			
+			<p><i class="fas fa-quote-left"></i> ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
+			ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+		</div>
+		<aside>
+			<div class="circle">
+				<img src="public/images/jean_forteroche.jpg" alt="portrait">
+			</div>
+		</aside>
+	</section>
+
+	<section id="table_of_contents">
+		<div class="chapter">
+			<h3>Chapitre 1</h3>
+			<p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Continuer la lecture</p>
+		</div>
+		<div class="chapter">
+			<h3>Chapitre 2</h3>
+			<p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Continuer la lecture</p>
+		</div>
+		<div class="chapter">
+			<h3>Chapitre 3</h3>
+			<p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Continuer la lecture</p>
+		</div>
+	</section>
+
+	<section id="back_cover">
+		
+	</section>
+
+	<footer>
+		
+	</footer>
 
 	<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
-	<script src="js/menu.js"></script>
+	<script src="public/js/menu.js"></script>
 </body>
 </html>
