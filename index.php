dbe3f746bbb591b95f8aad9b3b5a1131098bbb8c
Cavatz Natacha <natacha.cavatz@gmail.com>
2018-08-03 15:31:54 +0200

    Affichage des posts en récupérant les informations de la base de donnée

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+require('model/model.php');
+
+$req = getPosts();
 
 require('view/frontend/indexView.php');
 
--- view/frontend/indexView.php
+++ view/frontend/indexView.php
@@ -20,36 +20,23 @@
 
 <section id="table_of_contents">
 	<h2>Table des matières</h2>
+	<?php
+	while ($data = $req->fetch())
+	{
+	?>
 	<div class="chapter">
-		<h3>Chapitre 1</h3>
-		<p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<h3>
+			<?php echo htmlspecialchars($data['title']); ?>
+		</h3>
+		<p>
+			<?php echo htmlspecialchars($data['excerpt']); ?>
+		</p>
 		<p>Continuer la lecture</p>
-	</div>
-	<div class="chapter">
-		<h3>Chapitre 2</h3>
-		<p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<p>Continuer la lecture</p>
-	</div>
-	<div class="chapter">
-		<h3>Chapitre 3</h3>
-		<p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<p>Continuer la lecture</p>
-	</div>
+	</div>	
+	<?php
+	}
+	$req -> closeCursor();
+	?>
 </section>
 
 <?php $content = ob_get_clean(); ?>
