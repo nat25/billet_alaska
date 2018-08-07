b37559b05c5781afecbeff13536696da286519ac
Cavatz Natacha <natacha.cavatz@gmail.com>
2018-08-07 10:41:13 +0200

    Ajouter des commentaires

--- view/frontend/indexView.php
+++ view/frontend/indexView.php
@@ -39,6 +39,27 @@
 	?>
 </section>
 
+<section id="back_cover">
+	<div id="img_cover">
+		<img src="public/images/alaska.jpg">
+	</div>
+	<div id="text_cover">
+		<h2>Quatrième de couverture</h2>
+			<p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</section>
+
 <?php $content = ob_get_clean(); ?>
 
 <?php require('template.php'); ?>
--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -12,7 +12,22 @@
     </p>
 </div>
 
+
 <h2>Commentaires</h2>
+
+<form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
+    <div>
+        <label for="author">Auteur</label><br />
+        <input type="text" id="author" name="author" />
+    </div>
+    <div>
+        <label for="comment">Commentaire</label><br />
+        <textarea id="comment" name="comment"></textarea>
+    </div>
+    <div>
+        <input type="submit" />
+    </div>
+</form>
 
 <?php
 while ($comment = $comments->fetch())
@@ -23,6 +38,7 @@ while ($comment = $comments->fetch())
 <?php
 }
 ?>
+
 <?php $content = ob_get_clean(); ?>
 
 <?php require('template.php'); ?>
